99116118b6f2841cfdfb51bd0508a50efed70867
Admin <Admin@Admin-PC>
2016-11-21 15:26:36 +0530

    Adding the additional classes

---
+++ src/Test/Resorce/Addition.java
@@ -0,0 +1,15 @@
+package Test.Resorce;
+
+import junit.framework.TestCase;
+
+public class Addition extends TestCase {
+	
+	public int Calculate(int a, int b){
+		
+		int c = a+b;
+		return c;
+		
+	}
+	
+
+}
---
+++ src/Test/Resorce/Summing.java
@@ -0,0 +1,12 @@
+package Test.Resorce;
+
+
+import junit.framework.TestCase;
+
+
+public class Summing  extends TestCase{
+
+	Addition add = new Addition();
+
+		}
+
