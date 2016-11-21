3cf489a025f3adbce95609c9a03a111887ea7f2f
Admin <Admin@Admin-PC>
2016-11-21 15:25:16 +0530

    After Git creation done

--- src/Test/Resorce/StepFiles.java
+++ src/Test/Resorce/StepFiles.java
@@ -4,10 +4,16 @@ import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import junit.framework.TestCase;
 
-public class StepFiles {
+public class StepFiles extends TestCase {
 	
-	@Given("^Add Two numbers$")
+	
+	
+	public int a,b,c;
+	
+	@Given("^A"
+			+ "dd Two numbers$")
 	public void Add_Two_numbers() throws Throwable {
 	    // Express the Regexp above with the code you wish you had
 	    throw new PendingException();
@@ -17,13 +23,19 @@ public class StepFiles {
 	public void addition(int arg1, int arg2) throws Throwable {
 	    // Express the Regexp above with the code you wish you had
 	    throw new PendingException();
-	}
+	    
+		}
 
 	@Then("^the total sum should be (\\d+)$")
 	public void the_total_sum_should_be(int arg1) throws Throwable {
 	    // Express the Regexp above with the code you wish you had
-	    throw new PendingException();
-	}
+		
+		
+//	    throw new PendingException();
+		Addition Add = new Addition();
+		assertEquals(5, Add.Calculate(2,3));
+	    
+	 	}
 
 
 }
--- src/Test/Resorce/example.feature
+++ src/Test/Resorce/example.feature
@@ -5,6 +5,5 @@ I should be able to calculate the sum of two numbers
 
 Scenario:Calculate sum of numbers
 
-Given Add Two numbers 
-When 2 addition 3
+Given "2,3" 
 Then the total sum should be 5
