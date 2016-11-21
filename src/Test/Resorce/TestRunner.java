d98c0bdb6e4000217a88ca5628257c4b4bb8d8b9
Admin <Admin@Admin-PC>
2016-11-21 12:09:40 +0530

    First Commit

---
+++ src/Test/Resorce/TestRunner.java
@@ -0,0 +1,13 @@
+package Test.Resorce;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+@RunWith(Cucumber.class)
+@CucumberOptions(features="src/Test/Resorce")
+public class TestRunner {
+
+	
+
+}
